651ca289c1c5c6e477066f520d8f454811bf41ca
Peter-John Welcome <pjwelcome@gmail.com>
2018-05-27 21:24:53 +0200

    Initial commit

---
+++ app/src/main/java/com/multimeleon/pjapples/unittest_refactor_exerise/models/SearchResponse.java
@@ -0,0 +1,32 @@
+package com.multimeleon.pjapples.unittest_refactor_exerise.models;
+
+import android.support.annotation.Nullable;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class SearchResponse {
+    @Expose
+    private Integer totalCount;
+    @SerializedName("incomplete_results")
+    @Expose
+    private Boolean incompleteResults;
+    @SerializedName("items")
+    @Expose
+    private List<SearchResult> searchResults = null;
+
+    public Integer getTotalCount() {
+        return totalCount;
+    }
+
+    public Boolean getIncompleteResults() {
+        return incompleteResults;
+    }
+
+    @Nullable
+    public List<SearchResult> getSearchResults() {
+        return searchResults;
+    }
+}
